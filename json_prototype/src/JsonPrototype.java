5678d1cdfca2628486ba71cbe1dc804153ff3606
gormed <hans.ferchland@gmx.de>
2013-04-17 23:37:39 +0200

    basic concept for http json requsets with event prototype
    Signed-off-by: gormed <hans.ferchland@gmx.de>

--- json_prototype/src/JsonPrototype.java
+++ json_prototype/src/JsonPrototype.java
@@ -1,8 +1,11 @@
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.sql.Date;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
@@ -12,18 +15,42 @@ import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.json.JSONObject;
 
-
 public class JsonPrototype {
-	public static void main(String[] args) {
-		
-	}
-	
-	public JsonPrototype() {
-		String serverUrl = "";
 
-		JSONObject jsonObject = new JSONObject();
+	static int TIMEOUT_MILLISEC = 10000; // = 10 second
+	static String SERVER_URL = "";
+	
+	enum EventType {
+		GPS, RFID, MOTION
+	}
+
+	JFrame frame;
+	JLabel response;
+
+	public static void main(String[] args) {
+
+		new JsonPrototype();
+	}
+
+	public JsonPrototype() {
+
+		AbstractEvent event = new AbstractEvent(EventType.GPS, new Date(
+				System.currentTimeMillis()),
+				"Hier koennte ihre Werbung stehen!");
 		
-		int TIMEOUT_MILLISEC = 10000;  // = 10 seconds
+		frame = new JFrame("JsonPrototype");
+		response = new JLabel("");
+		String s = "Sending " + event.type.name() + " Event @"
+				+ event.time.toString() + " with data " + event.eventData;
+		frame.add(new JLabel(s));
+		frame.add(response);
+		frame.setVisible(true);
+		
+		jsonHttpRequest(SERVER_URL, event.getJsonObject());
+	}
+
+	private void jsonHttpRequest(String serverUrl, JSONObject jsonObject) {
+
 		HttpParams httpParams = new BasicHttpParams();
 		HttpConnectionParams.setConnectionTimeout(httpParams, TIMEOUT_MILLISEC);
 		HttpConnectionParams.setSoTimeout(httpParams, TIMEOUT_MILLISEC);
@@ -31,17 +58,41 @@ public class JsonPrototype {
 
 		HttpPost request = new HttpPost(serverUrl);
 		try {
-			request.setEntity(new ByteArrayEntity(
-			    jsonObject.toString().getBytes("UTF8")));
+			request.setEntity(new ByteArrayEntity(jsonObject.toString()
+					.getBytes("UTF8")));
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		try {
 			HttpResponse response = client.execute(request);
+			
+			this.response.setText(response.toString());
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+
+	class AbstractEvent {
+
+		EventType type;
+		Date time;
+		String eventData;
+
+		public AbstractEvent(EventType type, Date time, String data) {
+			this.eventData = data;
+			this.time = time;
+			this.type = type;
+		}
+
+		public JSONObject getJsonObject() {
+			JSONObject object = new JSONObject();
+			object.append("type", type);
+			object.append("time", time);
+			object.append("data", eventData);
+			return object;
 		}
 	}
 }
