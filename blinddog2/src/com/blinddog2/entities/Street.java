6b5796d427bed85f9981f8dc20d9861b6cf967b1
daired <hady@khalifa.de>
2013-06-15 22:15:40 +0200

    last change blind dog

---
+++ blinddog2/src/com/blinddog2/entities/Street.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.blinddog2.entities;
+
+/**
+ *
+ * @author hady
+ */
+public class Street {
+    
+}
