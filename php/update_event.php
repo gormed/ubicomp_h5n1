a579495619f355681e3c57c7aa0461e8d0b43145
siftos <siftos@gmail.com>
2013-06-16 15:57:44 +0200

    php changes

--- php/update_event.php
+++ php/update_event.php
@@ -1,52 +1,31 @@
 <?php
- 
-/*
- * Following code will update a product information
- * A product is identified by product id (id)
- */
- 
-// array for JSON response
+require 'functions.php';
 $response = array();
- 
-// check for required fields
+require_once __DIR__ . '/db_connect.php';
+$db = new DB_CONNECT();
+
 if (isset($_POST['deviceid']) && isset($_POST['receiverid']) && isset($_POST['eventid']) && isset($_POST['content'])) {
-    $table = "d".$_POST['deviceid'];   
+    $table = "d" . get_device_id($_POST['deviceid']);   
     $deviceid = $_POST['deviceid'];
     $eventid = $_POST['eventid'];
     $content = $_POST['content'];
     $receiverid = $_POST['receiverid'];
  
-    // include db connect class
-    require_once __DIR__ . '/db_connect.php';
- 
-    // connecting to db
-    $db = new DB_CONNECT();
- 
-    // mysql update row with matched id
     $result = mysql_query("UPDATE $table SET content = '$content', receiverid = '$receiverid' WHERE eventid = $eventid AND deviceid = '$deviceid'");
  
-    // check if row inserted or not
     if ($result) {
-        // successfully updated
         $response["message"] = "Event successfully updated.";
         $response['deviceid'] = $deviceid;
         $response['eventid'] = $eventid;
- 
-        // echoing JSON response
         echo json_encode($response);
     } else {
-        // failed to update row
         $response["message"] = "Oops! An error occurred.";
         $response['deviceid'] = $deviceid;
         $response['eventid'] = $eventid;
-        // echoing JSON response
         echo json_encode($response);
     }
 } else {
-    // required field is missing
     $response["message"] = "Required field(s) is missing";
- 
-    // echoing JSON response
     echo json_encode($response);
 }
 ?>
